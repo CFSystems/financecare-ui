4ca43515c45dfd2eb2469ef15ac3bd7b54fd9d90
Felipe Montilla <felipe.montilla.s@outlook.com>
2020-02-12 17:45:47 -0300

    Atualização para PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com',
+  apiUrl: 'https://financecare-api.herokuapp.com/',
 
-  tokenWhitelistedDomains: [ new RegExp('algamoney-api.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('financecare-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
